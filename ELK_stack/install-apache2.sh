fda20f23e1dd45d53a852d7cad7d956d9463674e
Jonathan <jpradoar@gmail.com>
2017-05-29 19:58:14 -0300

    Create install-apache2.sh
    script to install apache and logstash

---
+++ ELK_stack/install-apache2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+############################
+# Testing on: ubuntu-trusty-64 3.13.0-117-generic
+############################
+
+# update and upgrade system
+apt-get update
+apt-get upgrade -y 
+
+# Install basic packages
+apt-get install vim wget net-tools mlocate strace nmap telnet ansible git apache2 -y
+
+# donwload logstash
+wget https://artifacts.elastic.co/downloads/logstash/logstash-5.3.2.deb -O /tmp/logstash-5.3.2.deb
+
+# Create a basic index to test
+echo "<html><h1> Webserver base </h1><html>" >/var/www/html/index.html
+
+# Add repo and install java
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get install openjdk-8-jdk -y
+
+# Install logstash
+dpkg --install /tmp/logstash-5.3.2.deb
+
+# Start apache service
+/etc/init.d/apache2 start
+
+# download logstash apache config
+wget https://github.com/jpradoar/GaMTools/blob/master/ELK_stack/logstash-apache.conf /etc/logstash/conf.d/logstash-apache.conf 
+
+# start logstash
+/usr/share/logstash/bin/logstash  -f /etc/logstash/conf.d
